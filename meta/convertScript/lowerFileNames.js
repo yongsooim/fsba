4300c65c73892c4df9815c22a70e4e6306db2e1a
yongsoo im <yongsooim90@gmail.com>
2022-03-29 13:48:04 +0900

    lowering case

---
+++ meta/convertScript/lowerFileNames.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const { exec } = require("child_process");
+
+
+
+
+const testFolder = '../../ogg/se_event/';
+
+fs.readdir(testFolder, (err, files) => {
+    files.forEach(file => {
+
+        exec("git mv --force " + testFolder + file + " " + testFolder+file.toLowerCase(), (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+
+    });
+});
+
+
+
