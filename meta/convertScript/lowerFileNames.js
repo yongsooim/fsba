d90ac6b9f245ed2afa12e17bc4c524f328c4ef30
yongsoo im <yongsooim90@gmail.com>
2022-06-29 20:16:57 +0900

    add wav

--- meta/convertScript/lowerFileNames.js
+++ meta/convertScript/lowerFileNames.js
@@ -1,28 +1,46 @@
 const fs = require('fs');
 const { exec } = require("child_process");
 
+const testFolders = [
+    '../../wav/bgm/',
+    '../../wav/se_event/',
+    '../../wav/wav_eft/',
+];
+
+testFolders.forEach(testFolder => {
+    fs.readdir(testFolder, (err, files) => {
+        files.forEach(file => {
+
+            // for git
+            //exec("git mv --force " + testFolder + file + " " + testFolder+file.toLowerCase(), (error, stdout, stderr) => {
+            //    if (error) {
+            //        console.log(`error: ${error.message}`);
+            //        return;
+            //    }
+            //    if (stderr) {
+            //        console.log(`stderr: ${stderr}`);
+            //        return;
+            //    }
+            //    console.log(`stdout: ${stdout}`);
+            //});
 
 
-
-const testFolder = '../../mapset/json/';
-
-fs.readdir(testFolder, (err, files) => {
-    files.forEach(file => {
-
-        exec("git mv --force " + testFolder + file + " " + testFolder+file.toLowerCase(), (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
+            //for windows
+            exec("rename " + testFolder + file + " " + testFolder+file.toLowerCase(), (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
         });
-
     });
-});
+})
+
 
 
 
